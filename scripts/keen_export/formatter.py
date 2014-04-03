17647f407a492e8f6ff74d50564d65d55794f13a
Jason Gornall <jason.gornall@lessthan3.com>
2014-04-02 17:53:20 -0700

    update to formatter

--- scripts/keen_export/formatter.py
+++ scripts/keen_export/formatter.py
@@ -36,6 +36,8 @@ class formatter:
                 arr=re.findall("(?<="+agentString['os']['name']+")[^;]*",raw)
                 if (len(arr)==1):
                     agentString['os']['version']=arr[0].strip()
+        if ('os' in agentString and 'name' in agentString['os']):
+            agentString['os']['version']=agentString['os']['version'].strip()
         return agentString
 
 
@@ -201,6 +203,18 @@ class formatter:
             user_agent_raw=re.sub("\(.+?\)", '', user_agent_raw)
             user_agent_split=user_agent_raw.split()
             agentString=httpagentparser.detect(array_elements[1])
+            
+            engagedData={}
+            if ('engaged_10' in json_elements):
+                engagedData['10'] = json_elements['engaged_10']
+            if ('engaged_30' in json_elements):
+                engagedData['30'] = json_elements['engaged_30']
+            if ('engaged_60' in json_elements):
+                engagedData['60'] = json_elements['engaged_60']
+            if ('engaged_300' in json_elements):
+                engagedData['300'] = json_elements['engaged_300']
+            if ('engaged_600' in json_elements):
+                engagedData['600'] = json_elements['engaged_600']
 
             data = [{
             "data": agentString,
@@ -226,19 +240,14 @@ class formatter:
             },
             "elapsed": json_elements['elapsed'],
             "permanent_tracker": "??????????????",
-            "engaged": 
-            {
-                "10": json_elements['engaged_10'],
-                "30": json_elements['engaged_30'],
-                "60": json_elements['engaged_60']
-            },
+            "engaged": engagedData,
             "page": {
                 "_id": json_elements['page_id'],
                 "type": json_elements['page_type'],
                 "slug": json_elements['page_slug']
             },
         	}]
-              #engine
+            #engine
             if (len(user_agent_split)>1 and '/' in user_agent_split[1]):
                 split=user_agent_split[1].split('/')
                 engine_name=split[0]
@@ -323,14 +332,13 @@ class formatter:
     def formatPageViewData(text=""):
         #Application of data
         data=[{}]
-        #pprint(text)
+
         ##get array elements
         array_elements=re.findall("(?<=\[).*?(?=\])",text)
         
         text=re.sub("\[.+?\]", '', text)
 
         #get json elements
-
         temp=re.findall("\{.+?\}",text)
         text=re.sub("\{.+?\}", '', text)
 
@@ -349,6 +357,7 @@ class formatter:
             user_agent_paren=re.findall("(?<=\().*?(?=\))",user_agent_raw)
             user_agent_raw=re.sub("\(.+?\)", '', user_agent_raw)
             user_agent_split=user_agent_raw.split()
+            agentString=httpagentparser.detect(array_elements[1])
 
 
             data = [{
@@ -377,73 +386,60 @@ class formatter:
                 "slug": json_elements['page_slug']
             }
             }]
-            if (not 'Mobile' in user_agent_raw):
-                #engine
-                if (len(user_agent_split)>1 and '/' in user_agent_split[1]):
-                    split=user_agent_split[1].split('/')
-                    engine_name=split[0]
-                    engine_version=split[1]
-                elif ('/' in user_agent_split[0]):
-                    split=user_agent_split[0].split('/')
-                    engine_name=split[0]
-                    engine_version=split[1]
+            #engine
+            if (len(user_agent_split)>1 and '/' in user_agent_split[1]):
+                split=user_agent_split[1].split('/')
+                engine_name=split[0]
+                engine_version=split[1]
+            elif ('/' in user_agent_split[0]):
+                split=user_agent_split[0].split('/')
+                engine_name=split[0]
+                engine_version=split[1]
 
-                #browser
-                browser_name=""
-                browser_version=""
-                browser_major=[""]
-                if (len(user_agent_split)>3):
-                    split=user_agent_split[3].split('/')
-                    split_2=user_agent_split[2].split('/')
-                    browser_name=split[0]
-                    browser_version=split_2[1]
-                    browser_major=re.findall("^\d+(?=\.)",split_2[1])
-                elif (len(user_agent_split)>2):
-                    split=user_agent_split[2].split('/')
-                    browser_name=split[0]
-                    if (len(split)>1):
-                        browser_version=split[1]
-                        browser_major=re.findall("^\d+(?=\.)",split[1])
-                elif (len(user_agent_split)>0):
-                    split=user_agent_split[0].split('/')
-                    browser_name=split[0]
-                    if (len(split)>1):
-                        browser_version=split[1]
-                        browser_major=re.findall("^\d+(?=\.)",split[1])
+            # browser processing
+            browser_major=""
+            browser_name=""
+            browser_version=""
+            if ('browser' in agentString):
+                browser_name=agentString['browser']['name']
+                if ('version' in agentString['browser']):
+                    agentString['browser']['version']=agentString['browser']['version'].replace('_','.')
+                    split=agentString['browser']['version'].split('.')
+                    browser_version=agentString['browser']['version']
+                    if (split):
+                        browser_major=split[0] if len(split)>0 else ""
 
-                #OS
-                # grab first 4_4_4 or 2.2.2
-                os_raw=user_agent_paren[0]
-                pprint('-----')
-                pprint(os_raw)
-                os_split=re.findall("[\d][\d\._]+[\d]",os_raw)
-                os_version=""
-                if (len(os_split)>0):
-                    os_version=os_split[0]
-                    os_version=re.sub('_','.',os_version)
-                    os_raw=re.sub("[\d][\d\._]+[\d]","",os_raw)
-                    os_raw=re.sub("  "," ",os_raw)
+            
+            # os processing
+            os_name=""
+            os_version=""
+            pprint(agentString)
+            agentString=formatter.augmentAgentString(agentString,array_elements[1])
+            if ('os' in agentString):
+                #make all of them period based
+                if ('version' in agentString['os']):
+                    agentString['os']['version']=agentString['os']['version'].replace('_','.')
+                    os_version=agentString['os']['version']                   
+                   
+                os_name=agentString['os']['name']
 
-                os_name=os_raw
-                if ';' in os_raw:
-                    os_pieces=os_raw.split(';')
-                    os_name=os_pieces[1].strip()
 
-                data[0]["user_agent"] =  {
-                    "engine": {
-                        "version": engine_version,
-                        "name": engine_name
-                    },
-                    "os": {
-                        "version": os_version,
-                        "name": os_name
-                    },
-                    "browser": {
-                        "major": browser_major[0],
-                        "version": browser_version,
-                        "name": browser_name
-                    }
+            data[0]["user_agent"] =  {
+                "engine": {
+                    "version": engine_version,
+                    "name": engine_name
+                },
+                "os": {
+                    "version": os_version,
+                    "name": os_name
+                },
+                "browser": {
+                    "major": browser_major,
+                    "version": browser_version,
+                    "name": browser_name
                 }
+            }
+
             #we don't always have a referrer
             if (len(remaining_elements)>4):
                 ref_str=(remaining_elements[4].split(','))
